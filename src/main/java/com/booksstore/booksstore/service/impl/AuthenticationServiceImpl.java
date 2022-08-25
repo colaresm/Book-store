ec7bef3ec51db2a21ac195870127e89ee9f33857
colaresm <colaresmarcelo2018@gmail.com>
2022-08-25 19:45:07 -0300

    start login

--- src/main/java/com/booksstore/booksstore/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/booksstore/booksstore/service/impl/AuthenticationServiceImpl.java
@@ -1,7 +1,16 @@
 package com.booksstore.booksstore.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.booksstore.booksstore.service.AuthenticationService;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
-    
+    @Autowired
+    public AuthenticationServiceImpl(JwtTokenManager jwtTokenManager, UserDetailsService userDetailsService,
+                                     UserService userService, ProfessionalService professionalService) {
+        this.jwtTokenManager = jwtTokenManager;
+        this.userDetailsService = userDetailsService;
+        this.userService = userService;
+        this.professionalService = professionalService;
+    }
 }
